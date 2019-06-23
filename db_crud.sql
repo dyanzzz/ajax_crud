ee83f8311e67855bebaa1e7d3baea89540a6dbd8
Dyanzzz <dyanzzz@gmail.com>
2019-06-23 21:12:37 +0700

    Upload ajax_crud to git

---
+++ db_crud.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v12.4.3 (64 bit)
+MySQL - 10.1.16-MariaDB : Database - db_crud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_crud` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_crud`;
+
+/*Table structure for table `persons` */
+
+DROP TABLE IF EXISTS `persons`;
+
+CREATE TABLE `persons` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `gender` enum('male','female') DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `persons` */
+
+insert  into `persons`(`id`,`firstName`,`lastName`,`gender`,`address`,`dob`) values 
+(6,'budian','anto','male','serang','2019-06-03');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
